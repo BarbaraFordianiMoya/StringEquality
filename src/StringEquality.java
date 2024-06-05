4801eb6e27d656265bcc5f7f8cee0f0d46f7a3e1
Aluno <Aluno@10.85.198.10>
2024-06-05 16:46:57 -0300

    StringEquality

---
+++ src/StringEquality.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class StringEquality {
+   
+    String name = JOptionPane.showInputDialog("Coloque um nome:");
+    boolean l = name.equals("Moe");
+}
+
+/**/
